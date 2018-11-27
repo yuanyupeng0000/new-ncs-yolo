e597b6ba07d7d56b04d15c8b6b86b654cae6479a
yuanyupeng0000 <yuanyupeng-1989@163.com>
2018-11-27 23:01:53 +0800

    add get_xml_from_images file

--- truncated_get_xml_6cls_from_images.py
+++ truncated_get_xml_6cls_from_images.py
@@ -1,7 +1,7 @@
 from ctypes import *
 import math
 import random
-import sys
+import sys, os
 #cv2_root = "/usr/local/lib-bk/python2.7/dist-packages/cv2"
 #sys.path.insert(0,cv2_root)
 #cv2_lib_avcodec = "/usr/lib/x86_64-linux-gnu/bkavcodec"
@@ -9,10 +9,10 @@ import sys
 import cv2
 
 if(len(sys.argv) < 8):
-    print("please input data_file cfg_file weights_file save_start_index save_fram_gap video_file and total_fram_num from cmdline by order!")
+    print("please input data_file cfg_file weights_file save_start_index save_fram_gap images_dir and total_fram_num from cmdline by order!")
     exit(1)
 total_fram_num = int(sys.argv[7])
-video_file = sys.argv[6]
+images_dir = sys.argv[6]
 data_file = sys.argv[1]
 cfg_file = sys.argv[2]
 weights_file = sys.argv[3]
@@ -87,7 +87,7 @@ def detect(net, meta, image, thresh=.5, hier_thresh=.5, nms=.45):
     free_detections(dets, num)
     return res
 #lib = CDLL("/home/pjreddie/documents/darknet/libdarknet.so", RTLD_GLOBAL)
-lib = CDLL("./libdarknet.so", RTLD_GLOBAL)
+lib = CDLL("libdarknet.so", RTLD_GLOBAL)
 lib.network_width.argtypes = [c_void_p]
 lib.network_width.restype = c_int
 lib.network_height.argtypes = [c_void_p]
@@ -164,21 +164,17 @@ if __name__ == "__main__":
     #r = classify(net, meta, im)
     #print r[:10]
     net = load_net(cfg_file, weights_file, 0)
+    #net = load_net(b"/data/darknet/cfg/yolov3.cfg",b"/data/darknet/weights/yolov3.weights", 0)
     meta = load_meta(data_file)
     
-    cap = cv2.VideoCapture(video_file)
+    #cap = cv2.VideoCapture(video_file)
+    images = os.listdir(images_dir)
 
-    #image_index = 0
-    #gap = 0
-
-    # Check if camera opened successfully
-    if (cap.isOpened()== False): 
-        print("Error opening video stream or file")
-
-    # Read until video is completed
-    while(cap.isOpened()):
-        ret, frame = cap.read()
+    for image in images:
+        ret = image.endswith('.jpg')
         if ret == True:
+            image_file = images_dir + image
+
             if int(image_index%gap) == 0:
                 
                 #frame = frame[180:, 120:1720, :] #27.11.64.148_01_20180920120811979.mp4;
@@ -192,95 +188,6 @@ if __name__ == "__main__":
                 frame[:370, 1000:1350, :] = dst2 #27.11.64.149_01_20180920183555808.mp4
                                                  
                 '''
-
-                '''
-                img1 = frame[:180, :, :] #----------------
-                img2 = frame[:320, 1000:1350, :]
-                dst1 = cv2.blur(img1 , (16 ,32))
-                dst2 = cv2.blur(img2 , (16, 32))
-                frame[:180,:,:] = dst1
-                frame[:320, 1000:1350, :] = dst2 #27.11.64.149_01_20180920175502673.mp4
-                                                 #27.17.64.107_01_20180919183341690.mp4;
-
-                '''
-
-                '''
-                benchmark = int(image_index - image_index_start)/gap
-                print(benchmark)
-                if benchmark < 1000:
-                    img1 = frame[:380, :, :] #----------------
-                    #img2 = frame[:450, 500:780, :]
-                    dst1 = cv2.blur(img1 , (16 ,32))
-                    #dst2 = cv2.blur(img2 , (16, 32))
-                    frame[:380,:,:] = dst1
-                    #frame[:450, 500:780, :] = dst2 #27.11.64.149_01_20180920175502673.mp4
-                                                
-                else:
-                    img1 = frame[:380, :, :] #----------------
-                    img2 = frame[:450, 500:780, :]
-                    dst1 = cv2.blur(img1 , (16 ,32))
-                    dst2 = cv2.blur(img2 , (16, 32))
-                    frame[:380,:,:] = dst1
-                    frame[:450, 500:780, :] = dst2 #27.17.64.107_01_20180919183341690.mp4;
-
-                '''
-                '''
-                img1 = frame[:380, :, :] #----------------
-                img2 = frame[:450, 500:780, :]
-                dst1 = cv2.blur(img1 , (16 ,32))
-                dst2 = cv2.blur(img2 , (16, 32))
-                frame[:380,:,:] = dst1
-                frame[:450, 500:780, :] = dst2 #27.17.64.107_01_2018091918564462.mp4;
-                '''
-                '''
-                img1 = frame[:360, :, :]
-                dst1 = cv2.blur(img1 , (16 ,32))
-                frame[:360,:,:] = dst1
-                #27.17.64.107_01_20180920115125352.mp4;
-                '''
-                '''
-                img1 = frame[:300, :, :]
-                dst1 = cv2.blur(img1 , (16 ,32))
-                frame[:300,:,:] = dst1 #whsj
-                img1 = frame[:200, :, :]
-                dst1 = cv2.blur(img1 , (16 ,32))
-                frame[:200,:,:] = dst1
-                '''
-                
-                '''
-                img1 = frame[:340, :, :]
-                dst1 = cv2.blur(img1 , (16 ,32))
-                img2 = frame[:450, 780:1250, :]
-                dst2 = cv2.blur(img2, (16,32))
-                frame[:340,:,:] = dst1 #27.17.64.107_01_2018092017
-                frame[:450, 780:1250, :]  = dst2
-                '''
-
-                '''
-                img1 = frame[1150:, 520:2400, :]
-                frame = img1 #27.14.32.8_01_20180921181130405.mp4
-                '''
-
-                '''
-                temp_index = temp_index +1
-                if(temp_index%2 == 1):
-                    if(temp_index < 125):
-                        continue
-                    img1 = frame[600:1000, 1100:2000, :]
-                    frame = img1 #27.17.32.4_01_20180921174942256.mp4
-                else:
-                    img2 = frame[1000:, 1400:3400, :]
-                    frame = img2 #27.17.32.4_01_20180921174942256.mp4
-                ''' 
-                '''            
-                img1 = frame[1120:, 400:2731, :]
-                frame = img1 #27.11.32.3_01_20180921181804985.mp4
-                '''
-                            
-                '''
-                img1 = frame[300:, 200:-300, :]
-                frame = img1 #27.11.32.16_01_20180921183741410_1.mp4
-                '''
                 '''
                 temp_index = temp_index +1
                 if(temp_index%3 == 1):
@@ -293,47 +200,17 @@ if __name__ == "__main__":
                     img3 = frame[700:, 2048:, :]
                     frame = img3 #guangzhou_high_sky
                 '''
-                '''
-                img1 = frame[:50, :, :]
-                dst1 = cv2.blur(img1 , (16 ,32))
-                frame[:50,:,:] = dst1
-                '''
-                
-                '''
-                img1 = frame[:50, :320, :]
-                dst1 = cv2.blur(img1 , (16 ,32))
-                frame[:50, :320, :] = dst1 #
-                img2 = frame[:50, 320:500, :]
-                dst2 = cv2.blur(img2 , (16 ,32))
-                frame[:50, 320:500, :] = dst2 #
-                img3 = frame[:70, 500:, :]
-                dst3 = cv2.blur(img3 , (16 ,32))
-                frame[:70, 500:, :] = dst3 #
-                '''
-                '''
-                img1 = frame[270:370, 410:520, :]
-                dst1 = cv2.blur(img1 , (16 ,32))
-                frame[270:370, 410:520, :] = dst1 #
-                img2 = frame[:150, :250, :]
-                dst2 = cv2.blur(img2 , (16 ,32))
-                frame[:150, :250, :] = dst2 #
-                '''
-                            
+                r = detect(net, meta, image_file)
 
-                cv2.imwrite("image/" + str(image_index_start + (image_index-image_index_start)/gap).zfill(11) + ".jpg", frame)
-                fname = str(image_index_start + (image_index-image_index_start)/gap).zfill(11)
-                origimg_path = "image/" + fname + ".jpg"
-                r = detect(net, meta, origimg_path)
-
-                origimg = cv2.imread(origimg_path)
+                origimg = cv2.imread(image_file)
                 height = origimg.shape[0]
                 width = origimg.shape[1]
                 depth = origimg.shape[2]
 
                 xml_str = "<annotation>\n\t\
                 <folder>image</folder>\n\t\
-                <filename>" + origimg_path.split('/')[1] + "</filename>\n\t\
-                " + "<path>" + origimg_path + "</path>\n\t\
+                <filename>" + image + "</filename>\n\t\
+                " + "<path>" + image_file + "</path>\n\t\
                 <source>\n\t\t\
                 <database>Unknown</database>\n\t\
                 </source>\n\t\
@@ -381,7 +258,7 @@ if __name__ == "__main__":
                     title = "%s:%.2f" % (cls, score)
                     cv2.putText(origimg, title, p3, cv2.FONT_ITALIC, 0.6, (0, 255, 0), 1)
                 xml_str += "\n</annotation>"
-                fileObject = open("xml/" + fname + '.xml', 'w')  
+                fileObject = open("xml/" + image[:-4] + '.xml', 'w')  
                 fileObject.write(xml_str)   
                 fileObject.close()
                 print("Created : " + str(image_index_start + (image_index-image_index_start)/gap))
