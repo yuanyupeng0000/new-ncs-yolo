f45941f6582b96d26391700a9444e44e7ab2c0a9
yuanyupeng0000 <yuanyupeng-1989@163.com>
2019-01-30 17:10:25 +0800

    expect

--- shell_script/expect.sh
+++ shell_script/expect.sh
@@ -4,5 +4,18 @@ ip="192.168.1.219"
 name="root"
 
 ./annotation.exp $name $ip $video
-/data/darknet/python/rsync_xml_and_image_fit_server.sh
+
+#---------------------------------------------------------------------------
+
+date_time=`date "+%Y%m%d"`
+xml_dir=$date_time"_xml"
+image_dir=$date_time"_image"
+background_dir=$date_time"_background"
+mkdir -p $xml_dir
+mkdir -p $image_dir
+mkdir -p $background_dir
+
+./rsync_xml_and_image_fit_server.exp $date_time $xml_dir $image_dir $background_dir
+
+
 
